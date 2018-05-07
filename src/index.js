06536954be95f3b12e094763020e0bad6eb31834
Melanie Bockmann <melanie.bockmann@gmail.com>
2018-05-07 22:37:36 +1200

    page content - html + css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import 'bootstrap';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/main.scss';
+import orcaImgSrc from './img/orca.jpg';
+import belugaImgSrc from './img/beluga.jpg';
+import humpbackImgSrc from './img/humpback.jpg';
+
+const orcaImg = document.getElementById('img__orca');
+orcaImg.src = orcaImgSrc;
+
+const belugaImg = document.getElementById('img__beluga');
+belugaImg.src = belugaImgSrc;
+
+const humpbackImg = document.getElementById('img__humpback');
+humpbackImg.src = humpbackImgSrc;
+
+$(document).ready(function() {
+  $('.nav-link').click(function(e){
+    const url = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: $('section'+url+'').offset().top - 50
+    }, 500, 'swing');
+
+    e.preventDefault();
+  });
+});
